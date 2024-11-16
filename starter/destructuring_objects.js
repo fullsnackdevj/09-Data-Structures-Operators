e5ea6d00397258f3c3b9561a6f7f274eb8e73b65
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-11-16 13:49:51 +0800

    mutating variables with destructuring

--- starter/destructuring_objects.js
+++ starter/destructuring_objects.js
@@ -1,7 +1,7 @@
 // 'use strict';
 
 const restaurant = {
-  restauName: 'Classico Italiano',
+  name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
@@ -29,5 +29,36 @@ const restaurant = {
 
 // destructuring Objects
 
-const { restauName, openingHours, categories } = restaurant;
-console.log(restauName, openingHours, categories);
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// what if variables are not the same with the property names?
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+// dealing with 3rd party data, ie: api call.
+
+console.log(restaurant.menu); // undefined
+
+//default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+console.log(menu, starters);
+
+// mutating variables while destructuring
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+// {a, b} = obj; // Uncaught SyntaxError: Unexpected token '='
+
+({ a, b } = obj);
+
+console.log(a, b);
